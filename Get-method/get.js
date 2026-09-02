6cb1329eb1122dfd3aeba788b7f5e94aecdd9bf1
Aakash <aakash76k747@gmail.com>
2026-09-02 20:41:53 +0530

    update example in get method

--- Get-method/get.js
+++ Get-method/get.js
@@ -2,24 +2,47 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.get("/", (req , res)=>{
-    res.send("hello");
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
+// app.get("/about", (req , res)=>{
+//     res.send("about page");
+// });
 
-app.get("/about", (req , res)=>{
-    res.send("about page");
+// app.get("/api/contact",(req, res)=>{
+//     res.json({
+//        name: "jhon Doe",
+//        email: "john@gmail.com",
+//        phone: "123-456-7890",
+//        course: "Node js"
+//     });
+// })
+
+// app.listen(3000, () => {
+//   console.log("server is running on http://localhost:3000");
+// });
+
+// Example :- gym management system
+
+app.get("/about", (req, res) => {
+  res.json({
+    name: "Rahul",
+    age: "30",
+    height: "6",
+    weight: "76",
+  });
 });
 
-app.get("/api/contact",(req, res)=>{
-    res.json({
-       name: "jhon Doe",
-       email: "john@gmail.com",
-       phone: "123-456-7890",
-       course: "Node js" 
-    });
-})
+app.get("/contact", (req, res) => {
+  res.json({
+    name: "Gym",
+    address: "xyz",
+    location: "abcd",
+    contact: "1234567891",
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("server is running on http://localhost:3000");
+app.listen(3000, () => {
+  console.log("server is running on http://localhost:3000");
 });
