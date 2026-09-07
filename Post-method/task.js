149f90374a7efa672e197b4630cb3bce63b0d192
Aakash <aakash76k747@gmail.com>
2026-09-07 12:30:25 +0530

    update task

---
+++ Post-method/task.js
@@ -0,0 +1,36 @@
+
+const express = require("express");
+const app = express();
+const task1 = require("./task1");
+
+const gym = [];
+
+app.use(express.json());
+
+app.get("/api/gym" , (req, res)=>{
+  res.json(gym);
+})
+
+app.post("/api/gym", (req, res) => {
+  const memberName = req.body.name;
+  const memberPlan = req.body.plan;
+
+  console.log("new Data :  ", req.body);
+
+  const GymMember = {
+    name:memberName,
+    plan:memberPlan
+  }
+
+  gym.push(GymMember);
+
+  res.json({
+    success: true,
+    message: "Data received successfully",
+    GymMember
+  });
+});
+app.listen(7000, () => {
+  console.log("Server is running on port http://localhost:7000");
+});
+
---
+++ Post-method/task1.js
@@ -0,0 +1,22 @@
+let task1 = [
+    {
+        name: "chaman",
+        plan: "3 months",
+    },
+    {
+        name: "vishal",
+        plan: "5 months",
+    },
+    {
+        name: "pawan",
+        plan: "8 months",
+    },
+    {
+        name: "dhruv",
+        plan: "12 months",
+    }
+
+    
+]
+
+module.exports = task1;
