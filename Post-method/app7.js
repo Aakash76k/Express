a418a786f862e1c13ac966f40a789c4a8fe79048
Aakash <aakash76k747@gmail.com>
2026-09-08 12:41:10 +0530

    complete and update task in index.html and app7.js

--- Post-method/app7.js
+++ Post-method/app7.js
@@ -1,45 +1,47 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { json } = require("stream/consumers");
 const app = express();
 app.use(express.json());
 
-const filePath = "members.json";
+const filePath = "./members.json";
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(___dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/api/member", (req, res) => {
+app.post("/api/members", (req, res) => {
   const { name, plan } = req.body;
-  const newmember = {
+  const newMember = {
     id: Date.now(),
     name: name,
-    age: age,
+    plan: plan,
   };
 
-  fs.readFile(fileURLToPath, "utf-8", (err, fileData) => {
-    let memberArray = [];
-    if (err) {
+  fs.readFile(filePath, "utf-8", (err, fileData) => {
+    let membersArray = [];
+    if (!err) {
       try {
-        memberArray = json.parse(fileData);
+        membersArray = JSON.parse(fileData);
       } catch (parseError) {
         console.log(parseError);
       }
     }
     membersArray.push(newMember);
-    fs.writeFile(filePath, JSON.stringify(membersArray, null, 2), (error) => {
+
+    fs.writeFile(filePath, JSON.stringify(membersArray, null, 2), (err) => {
       if (err) {
-        return res.json("Error : ", err);
+        return res.json("Error  : ", err);
       }
       res.json({
-        message: "member ",
+        message: "member  ",
         member: newMember,
       });
     });
   });
 });
 
-app.listen(8000, () => {
-  console.log("server is running on http://localhost:8000");
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
 });
