d2edb8182aef4b9c81ff47e6a748e6447c07e228
Aakash <aakash76k747@gmail.com>
2026-09-10 12:08:27 +0530

    update middleware task3

--- Middleware/task3.js
+++ Middleware/task3.js
@@ -1,23 +1,25 @@
-// task :- check sale through time
+//task :- check gym maintainance
 
 const express = require("express");
 const app = express();
 
 
 const date = new Date();
-const time = date.getHours();
-//predefine middleware use query // url?role=Admin
+
+
 
 app.use((req,res,next)=>{
     next();
 })
 
 
-const checkTime = (req,res,next)=>{
-    if(time>7 && time<24){
+
+const maintain = (req,res,next)=>{
+    const maintain = true;
+    if(maintain){
         next();
     }else{
-        res.send("time is over")
+        res.send("gym closed")
     }
 }
 
@@ -25,8 +27,8 @@ app.get("/", (req, res) => {
   res.send("Hello Guru ji");
 });
 
-app.get("/api/sale",checkTime,(req,res,next)=>{
-    res.send("sale is live")
+app.get("/api/maintain",maintain,(req,res,next)=>{
+    res.send("Gym open")
 })
 
 
