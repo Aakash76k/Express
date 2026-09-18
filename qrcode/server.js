41ae86e0d220e86ec79f5f8808bfcaf3365583d7
Aakash <aakash76k747@gmail.com>
2026-09-18 12:19:44 +0530

    update qr code

--- qrcode/server.js
+++ qrcode/server.js
@@ -1,33 +1,40 @@
-const express = require('express');
-const QRCode = require('qrcode');
+const express = require("express");
+const QRCode = require("qrcode");
 const app = express();
+const fs = require("fs");
+const path = require("path");
 
-app.get("/",(req,res)=>{
-    res.send("Home");
+const qrFolder = path.join(__dirname, "qr_image");
+if (!fs.existsSync(qrFolder)) {
+  fs.mkdirSync(qrFolder);
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/qrcode", async (req, res) => {
+  const textOrUrl = req.query.text;
 
-app.get("/qrcode" ,async (req,res)=>{
-    const textOrUrl = req.query.text;
+  if (!textOrUrl) {
+    return res.status(400).send("invalid data");
+  }
 
-    if(!textOrUrl){
-        return res.status(400).send("invalid data")
-    }
+  try {
+    const fileName = `qrCode_${Date.now()}.png`;
+    const filePath = path.join(qrFolder, fileName);
 
-    try{
-        const qrbufferImage = await QRCode.toBuffer(textOrUrl)
-        res.setHeader('content-type','image/png')
-        res.send(qrbufferImage)
-
-    }
-
-    catch(error){
-        console.log(error);
-    }
-    res.send("Error")
-
+    await QRCode.toFile(filePath, textOrUrl);
+    res.json({
+      message: "Qr code save successfully",
+      fileName: fileName,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send("Server Error");
+  }
 });
 
-app.listen(4000,()=>{
-    console.log("http://localhost:4000");
+app.listen(4000, () => {
+  console.log("http://localhost:4000");
 });
